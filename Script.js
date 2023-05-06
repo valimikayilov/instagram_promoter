d0a23f94622bb08e5269e21bfc5b995e811f9eaf
Vali <valimikayilov@yahoo.com>
2023-05-07 00:45:24 +0200

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,12 @@
+# instagram_promoter
+
+time = prompt("Рекомендованый интервал от 32 до 36 сек", "33");
+a = document.getElementsByClassName("_5f5mN");
+b = document.getElementsByClassName("aOOlW");
+function insta() {
+if (a[0]){a[0].click()}
+if (b[0]){b[0].click()}
+}
+window.open('https://neirobot.com','_blank');
+setInterval(insta, time * 1000);
+console.log("Накрутка пошла!");
